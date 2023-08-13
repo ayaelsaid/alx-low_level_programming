d9de1bb892a73c66ae89086128b4c02cd3a143b4
ayalsaid <ayae7675@gmail.com>
2023-08-12 23:52:11 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ *
+ * Description: declare the value
+ *
+ * Return: 0(success)
+*/
+
+int main(void)
+{
+	int y, j;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (j = y + 1; j <= 9; j++)
+		{
+			putchar(48 + y);
+			putchar(48 + j);
+
+			if (y < 8 || j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
