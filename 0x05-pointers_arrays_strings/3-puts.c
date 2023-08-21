e4dd1b2fe8a864d31ec8da17bd9f50e0ceeac9fa
ayalsaid <ayae7675@gmail.com>
2023-08-21 14:35:38 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ *
+ * @str: take a string
+ *
+ * Return: void
+*/
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
