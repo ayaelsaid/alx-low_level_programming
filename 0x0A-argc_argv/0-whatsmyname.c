8e6315ca2cbbe86ee9dc5830765343673208aba0
ayalsaid <ayae7675@gmail.com>
2023-09-01 15:16:47 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: take a number
+ *
+ * @argv: take a string
+ *
+ * Return: 0
+*/
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
