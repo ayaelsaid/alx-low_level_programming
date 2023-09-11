2925ab91d8825e66826aefd0d6eeef5bad8fc340
ayalsaid <ayae7675@gmail.com>
2023-09-11 11:43:35 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new type
+ * @name: string
+ * @age: float
+ * @owner: string
+ * description: declare the struct
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
