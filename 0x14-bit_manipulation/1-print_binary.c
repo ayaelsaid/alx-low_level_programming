5f8024f0042f9d6deee1844a6ccdfcc46be6e394
ayaelsaid <ayae7675@gmail.com>
2023-09-29 17:48:42 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - ...
+ * @n: ...
+ * Return: ...
+*/
+void print_binary(unsigned long int n)
+{
+	int i, flag;
+	char bit;
+	int bitscount = sizeof(unsigned long int) * 8;
+
+	if (n == 0)
+		printf("0");
+	flag = 0;
+	for (i = bitscount - 1; i >= 0; i--)
+	{
+	bit = (n >> i) & 1;
+	if (flag == 1)
+	{
+	putchar(bit + '0');
+	}
+	else if (bit == 1)
+	{
+		putchar(bit + '0');
+		flag = 1;
+	}
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+void print_binary(unsigned long int n);
 
 #endif
