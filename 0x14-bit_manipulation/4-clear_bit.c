15dcde23d35fd93fd162d53d41b47d3cc4bb9a60
ayaelsaid <ayae7675@gmail.com>
2023-10-01 17:18:30 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * clear_bit - ...
+ * @n: ...
+ * @index: ...
+ * Return: ...
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	mask = 1 << index;
+	*n &= ~mask;
+	return (1);
+}
