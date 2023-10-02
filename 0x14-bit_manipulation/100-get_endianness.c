aa395979f396a2c3198ca69bdb291a0670fde9bb
ayaelsaid <ayae7675@gmail.com>
2023-10-01 18:48:01 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - ...
+ * Return: ...
+*/
+int get_endianness(void)
+{
+	int num = 1;
+	int *n;
+
+	n = &num;
+	if (*n == 1)
+		return (1);
+	else
+		return (0);
+}
